fcaccd61398baaaaab7c4d0284979406df04b743
ahmad-mhajna <ahmad.mhmad.rashed777@gmail.com>
2022-04-05 12:12:19 +0300

    1.1

---
+++ 1.1-primitive data types/java.js
@@ -0,0 +1,9 @@
+let string = "hello world";
+let number = 4;
+let boolean = true;
+let OR = null;
+let undef = undefined;
+let obj = {};
+let array = [];
+let sl = Symbol("hello");
+console.log(typeof string);
