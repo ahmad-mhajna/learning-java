301862af4b74e32d9e2625d34413a56f3e452ae8
ahmad-mhajna <ahmad.mhmad.rashed777@gmail.com>
2022-04-13 17:24:17 +0300

    small fix

--- 12.3-.school_method/java.js
+++ 12.3-.school_method/java.js
@@ -37,36 +37,37 @@ const school = {
       age: 21,
     },
   ],
-};
-function findPerson(isTeacher, id) {
-  if (isTeacher) return school.teachers.find((teacher) => teacher.id === id);
-  else return school.students.find((student) => student.id === id);
-}
-function assignStudent(id, subject) {
-  const student = findPerson(false, id);
-  const teacher = school.teachers.find((teacher) =>
-    teacher.subjects.includes(subject)
-  );
-  if (teacher.capacityLeft > 0) {
-    teacher.students.push(student);
-    teacher.capacityLeft--;
+  findPerson(isTeacher, id) {
+    if (isTeacher) return this.teachers.find((teacher) => teacher.id === id);
+    else return this.students.find((student) => student.id === id);
+  },
+  assignStudent(id, subject) {
+    const student = this.findPerson(false, id);
+    const teacher = this.teachers.find((teacher) =>
+      teacher.subjects.includes(subject)
+    );
+    if (teacher.capacityLeft > 0) {
+      teacher.students.push(student);
+      teacher.capacityLeft--;
+      console.log(teacher);
+    } else
+      console.log(`Sorry,
+    no available teachers left`);
+  },
+  assignTeachersSubject(id, subject) {
+    const teacher = this.findPerson(true, id);
+    if (!teacher.subjects.includes(subject)) teacher.subjects.push(subject);
     console.log(teacher);
-  } else
-    console.log(`Sorry,
-  no available teachers left`);
-}
-const assignTeachersSubject = (id, subject) => {
-  const teacher = findPerson(true, id);
-  if (!teacher.subjects.includes(subject)) teacher.subjects.push(subject);
-  console.log(teacher);
-};
-const removeTeachersSubject = (id, subject) => {
-  const teacher = findPerson(true, id);
-  let subIndex = teacher.subjects.indexOf(subject);
-  if (teacher.subjects.includes(subject)) teacher.subjects.splice(subIndex, 1);
-  console.log(teacher);
+  },
+  removeTeachersSubject(id, subject) {
+    const teacher = this.findPerson(true, id);
+    let subIndex = teacher.subjects.indexOf(subject);
+    if (teacher.subjects.includes(subject))
+      teacher.subjects.splice(subIndex, 1);
+    console.log(teacher);
+  },
 };
 
-assignStudent(11, `biology`);
-assignTeachersSubject(1, `english`);
-removeTeachersSubject(1, `english`);
+school.assignStudent(11, `biology`);
+school.assignTeachersSubject(1, `english`);
+school.removeTeachersSubject(1, `english`);
