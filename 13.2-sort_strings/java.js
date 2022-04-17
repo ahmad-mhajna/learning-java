79d07d46f44fb9a0c86a83593063b1819b377c33
ahmad-mhajna <ahmad.mhmad.rashed777@gmail.com>
2022-04-17 11:17:01 +0300

    fix

--- 13.2-sort_strings/java.js
+++ 13.2-sort_strings/java.js
@@ -1,19 +1,19 @@
 const foods = ["falafel", "sabich", "hummus", "pizza with extra pineapple"];
-const descsort = foods.reverse();
-console.log(descsort);
-const ascsort = foods.reverse();
-console.log(ascsort);
+const ascSort = foods.slice().sort();
+const desSort = foods.slice().sort().reverse();
 const foodsWithUpperCase = [
   "falafel",
   "Sabich",
   "hummus",
   "pizza with extra pineapple",
 ];
-foodsWithUpperCase.reverse();
-console.log(foodsWithUpperCase);
-foodsWithUpperCase.reverse();
-console.log(foodsWithUpperCase);
+const ascSortUpper = foodsWithUpperCase
+  .slice()
+  .sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
+const desSortUpper = foodsWithUpperCase
+  .slice()
+  .sort((a, b) => b.toLowerCase().localeCompare(a.toLowerCase()));
 
 const words = ["apple", "supercalifragilisticexpialidocious", "hi", "zoo"];
-const longestword = words.sort((a, b) => a.length - b.length);
+const longestword = words.slice().sort((a, b) => a.length - b.length);
 console.log(longestword);
